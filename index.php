9b9b0eef6df30af3047437418bf7816cf0c2e94b
alikamal234-gif <alikamal0675@gmail.com>
2025-12-25 17:10:12 +0100

    OCUCC-16 : l'ajoute de doctors avec la meme methode de l'ajoute de patient

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ class Menu
         echo "3. Modifucation \n";
         echo "4. La supprission \n";
         echo "5. Statistique \n";
+        echo "6. Exit \n";
 
         $choice = trim(fgets(STDIN));
         switch ($choice) {
@@ -42,154 +43,162 @@ class Menu
     }
 
     public function AjouteMenu()
-{
-    echo "========== Menu ====================\n";
-    echo "1. ajoute un patient \n";
-    echo "2. ajoute un medeciant \n";
-    echo "3. ajoute un departement \n";
-    $choiceAjoute = trim(fgets(STDIN));
-    switch ($choiceAjoute) {
-        case 1:
-            echo "First name : \n";
-            $firstName = trim(fgets(STDIN));
-            echo "Last name : \n";
-            $LastName = trim(fgets(STDIN));
-            echo "Email : \n";
-            $email = trim(fgets(STDIN));
-            echo "Phone Number : \n";
-            $phone = trim(fgets(STDIN));
-            echo "Gender : \n";
-            $gender = trim(fgets(STDIN));
-            echo "Date Of bithday : \n";
-            $birdth = trim(fgets(STDIN));
-            echo "Adress : \n";
-            $adress = trim(fgets(STDIN));
+    {
+        echo "========== Menu ====================\n";
+        echo "1. ajoute un patient \n";
+        echo "2. ajoute un medeciant \n";
+        echo "3. ajoute un departement \n";
+        $choiceAjoute = trim(fgets(STDIN));
+        switch ($choiceAjoute) {
+            case 1:
+                echo "First name : \n";
+                $firstName = trim(fgets(STDIN));
+                echo "Last name : \n";
+                $LastName = trim(fgets(STDIN));
+                echo "Email : \n";
+                $email = trim(fgets(STDIN));
+                echo "Phone Number : \n";
+                $phone = trim(fgets(STDIN));
+                echo "Gender : \n";
+                $gender = trim(fgets(STDIN));
+                echo "Date Of bithday : \n";
+                $birdth = trim(fgets(STDIN));
+                echo "Adress : \n";
+                $adress = trim(fgets(STDIN));
+                $patient = new patient($firstName, $LastName, $email, $phone, $gender, $birdth, $adress);
+                $patient->SetPerson();
+                $this->MenuPrincipal();
+                break;
+            case 2:
 
-            $patient = new patient($firstName, $LastName, $email, $phone, $gender, $birdth, $adress);
-            $patient->SetPerson();
-            echo "ajouter est successful 👀";
-            $this->MenuPrincipal();
-            break;
-        case 2:
-            echo "mamr7bax biiiik";
-            break;
-        case 3:
-            echo "name : \n";
-            $name = trim(fgets(STDIN));
-            echo "\nlocation : ";
-            $location = trim(fgets(STDIN));
-            $department = new departement($name, $location);
-            $department->setDepartement();
-            break;
-
+                echo "First name : \n";
+                $firstName = trim(fgets(STDIN));
+                echo "Last name : \n";
+                $LastName = trim(fgets(STDIN));
+                echo "Email : \n";
+                $email = trim(fgets(STDIN));
+                echo "Phone Number : \n";
+                $phone = trim(fgets(STDIN));
+                echo "Specialization : \n";
+                $specialization = trim(fgets(STDIN));
+                echo "id_department : \n";
+                $id_department = trim(fgets(STDIN));
+                $doctor = new doctor($firstName, $LastName, $email, $phone,$specialization,(int)$id_department);
+                $doctor->SetPerson();
+                $this->MenuPrincipal();
+                break;
+            case 3:
+                echo "name : \n";
+                $name = trim(fgets(STDIN));
+                echo "\nlocation : ";
+                $location = trim(fgets(STDIN));
+                $department = new departement($name, $location);
+                $department->setDepartement();
+                break;
+        }
     }
-}
     public function AfficheMenu()
-{
-    echo "========== Menu ====================\n";
-    echo "1. affiche un patient \n";
-    echo "2. affiche un medeciant \n";
-    echo "3. affiche un departement \n";
-    $choiceAffiche = trim(fgets(STDIN));
+    {
+        echo "========== Menu ====================\n";
+        echo "1. affiche un patient \n";
+        echo "2. affiche un medeciant \n";
+        echo "3. affiche un departement \n";
+        echo "3. Exit \n";
+        $choiceAffiche = trim(fgets(STDIN));
+        switch ($choiceAffiche) {
+            case 1:
+                $patients = new patient();
+                print_r($patients->GetPerson('patients'));
+                $this->MenuPrincipal();
+                break;
+            case 2:
+                $doctor = new doctor();
+                print_r($doctor->GetPerson('departments'));
+                $this->MenuPrincipal();
+                break;
+            case 3:
+                $departementss = new departement("ali", "youcode");
+                print_r($departementss->getDepartement());
+                $this->MenuPrincipal();
+                break;
+            case 4:
+                echo "Exit";
+                break;
 
-    switch ($choiceAffiche) {
-        case 1:
-            $patients = new patient();
-            print_r($patients->GetPerson('patients'));
-            $this->MenuPrincipal();
-            break;
-        case 2:
-            $doctor = new doctor();
-            print_r($doctor->GetPerson('departments'));
-            $this->MenuPrincipal();
-            break;
-        case 3:
-            $departementss = new departement("ali", "youcode");
-            print_r($departementss->getDepartement());
-            $this->MenuPrincipal();
-            break;
-        case 4:
-            $this->supprimeMenu();
-            break;
-        case 5:
-            echo "mamr7bax biiiik";
-            break;
-
+        }
     }
-}
 
 
-function modifieMenu()
-{
-    system('cls');
-    echo "========== Menu ====================\n";
-    echo "1. modifie un patient \n";
-    echo "2. modifie un medeciant \n";
-    echo "3. modifie un departement \n";
+    function modifieMenu()
+    {
+        system('cls');
+        echo "========== Menu ====================\n";
+        echo "1. modifie un patient \n";
+        echo "2. modifie un medeciant \n";
+        echo "3. modifie un departement \n";
 
-    $choicemodifie = trim(fgets(STDIN));
-    switch ($choicemodifie) {
-        case 1:
-            echo "mamr7bax biiiik";
-            break;
-        case 2:
-            echo "mamr7bax biiiik";
-            break;
-        case 3:
-            echo "ID ( departement ) : \n";
-            $id = trim(fgets(STDIN));
-            echo "Que veux-tu modufier ?\n1.name\n2.location\n";
-            $modufier = trim(fgets(STDIN));
-            $modofierChose = null;
+        $choicemodifie = trim(fgets(STDIN));
+        switch ($choicemodifie) {
+            case 1:
+                echo "mamr7bax biiiik";
+                break;
+            case 2:
+                echo "mamr7bax biiiik";
+                break;
+            case 3:
+                echo "ID ( departement ) : \n";
+                $id = trim(fgets(STDIN));
+                echo "Que veux-tu modufier ?\n1.name\n2.location\n";
+                $modufier = trim(fgets(STDIN));
+                $modofierChose = null;
 
-            if ($modufier == 1) {
-                $modofierChose = "department_name";
-            } elseif ($modofierChose == "location") {
-                $modofierChose = "location";
-            }
-            echo "change By : \n";
-            $change = trim(fgets(STDIN));
-            $department = new departement();
-            $department->ModifierDepartement($id, $modofierChose, $change);
-            break;
+                if ($modufier == 1) {
+                    $modofierChose = "department_name";
+                } elseif ($modofierChose == "location") {
+                    $modofierChose = "location";
+                }
+                echo "change By : \n";
+                $change = trim(fgets(STDIN));
+                $department = new departement();
+                $department->ModifierDepartement($id, $modofierChose, $change);
+                break;
 
+        }
     }
-}
 
 
-    function supprimeMenu()
-{
-    system('cls');
-    echo "========== Menu ====================\n";
-    echo "1. supprime un patient \n";
-    echo "2. supprime un medeciant \n";
-    echo "3. supprime un departement \n";
+    public function supprimeMenu()
+    {
+        echo "========== Menu ====================\n";
+        echo "1. supprime un patient \n";
+        echo "2. supprime un medeciant \n";
+        echo "3. supprime un departement \n";
 
-    $choicesupprime = trim(fgets(STDIN));
-    switch ($choicesupprime) {
-        case 1:
-            echo "mamr7bax biiiik";
-            break;
-        case 2:
-            echo "mamr7bax biiiik";
-            break;
-        case 3:
-            echo "ID ( departement ) : \n";
-            $id = trim(fgets(STDIN));
-            $department = new departement();
-            $name = $department->getDepartementId((int) $id);
-            echo "Verifier est-ce-que tu vaux supprime " . $name[0]['department_name'] . " (yes/no) :\n";
+        $choicesupprime = trim(fgets(STDIN));
+        switch ($choicesupprime) {
+            case 1:
+                echo "mamr7bax biiiik";
+                break;
+            case 2:
+                echo "mamr7bax biiiik";
+                break;
+            case 3:
+                echo "ID ( departement ) : \n";
+                $id = trim(fgets(STDIN));
+                $department = new departement();
+                $name = $department->getDepartementId((int) $id);
+                echo "Verifier est-ce-que tu vaux supprime " . $name[0]['department_name'] . " (yes/no) :\n";
 
-            $supprimeChose = trim(fgets(STDIN));
+                $supprimeChose = trim(fgets(STDIN));
 
-            if (strtoupper($supprimeChose) == "YES") {
-                $department->supprimeDepartment((int) $id);
-            } else {
-                echo "okk khod ra7tek ";
-            }
-            break;
+                if (strtoupper($supprimeChose) == "YES") {
+                    $department->supprimeDepartment((int) $id);
+                } else {
+                    echo "okk khod ra7tek ";
+                }
+                break;
+        }
     }
-}
 
 }
 
--- src/config.php
+++ src/config.php
@@ -29,21 +29,24 @@ class Person
 
     public $conection;
 
-    public function __construct($FirstName=null,$LastName=null,$email=null,$PhoneNumber=null) {
+    public function __construct($FirstName = null, $LastName = null, $email = null, $PhoneNumber = null)
+    {
         $this->FirstName = $FirstName;
         $this->LastName = $LastName;
         $this->email = $email;
         $this->PhoneNumber = $PhoneNumber;
     }
 
-    public function GetPerson($tableName){
-        $this->conection =  new database();
+    public function GetPerson($tableName)
+    {
+        $this->conection = new database();
         return $this->conection->conn->query("SELECT * FROM $tableName ;")->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function SetPerson($tableName){
-        $this->conection =  new database();
-        
+    public function SetPerson($tableName)
+    {
+        $this->conection = new database();
+
     }
 
 }
@@ -54,27 +57,34 @@ class patient extends Person
     public $DateOfBirth;
     public $adress;
 
-    public function __construct($FirstName=null,$LastName=null,$email=null,$PhoneNumber=null,$gender=null,$DateOfBirth=null,$adress=null) {
-        parent::__construct($FirstName,$LastName,$email,$PhoneNumber);
+    public function __construct($FirstName = null, $LastName = null, $email = null, $PhoneNumber = null, $gender = null, $DateOfBirth = null, $adress = null)
+    {
+        parent::__construct($FirstName, $LastName, $email, $PhoneNumber);
         $this->gender = $gender;
         $this->DateOfBirth = $DateOfBirth;
         $this->adress = $adress;
     }
 
 
-    public function SetPerson($tableName= 'patients'){
-        $this->conection =  new database();
-        $sql = "INSERT INTO $tableName (first_name,last_name,gender,email,date_of_birth,phone_number,address) 
+    public function SetPerson($tableName = 'patients')
+    {
+        try {
+            $this->conection = new database();
+            $sql = "INSERT INTO $tableName (first_name,last_name,gender,email,date_of_birth,phone_number,address) 
         VALUES ( :first_name, :last_name, :gender, :email, :date_of_birth, :phone_number , :adress); ";
-        $stm = $this->conection->conn->prepare($sql);
-        $stm->bindParam('first_name',$this->FirstName);
-        $stm->bindParam('last_name',$this->LastName);
-        $stm->bindParam('gender',$this->gender);
-        $stm->bindParam('email',$this->email);
-        $stm->bindParam('date_of_birth',$this->DateOfBirth);
-        $stm->bindParam('phone_number',$this->PhoneNumber);
-        $stm->bindParam('adress',$this->adress);
-        $stm->execute();
+            $stm = $this->conection->conn->prepare($sql);
+            $stm->bindParam('first_name', $this->FirstName);
+            $stm->bindParam('last_name', $this->LastName);
+            $stm->bindParam('gender', $this->gender);
+            $stm->bindParam('email', $this->email);
+            $stm->bindParam('date_of_birth', $this->DateOfBirth);
+            $stm->bindParam('phone_number', $this->PhoneNumber);
+            $stm->bindParam('adress', $this->adress);
+            $stm->execute();
+            echo "ajouter est successful 👀\n";
+        }catch(Exception $ERROR){
+            echo 'error de ajoute !!';
+        }
 
     }
 }
@@ -84,7 +94,34 @@ class doctor extends Person
     public $specialization;
     public $IdDepartement;
 
-    
+    public function __construct($FirstName = null, $LastName = null, $email = null, $PhoneNumber = null, $specialization = null, $IdDepartement = null)
+    {
+        parent::__construct($FirstName, $LastName, $email, $PhoneNumber);
+        $this->specialization = $specialization;
+        $this->IdDepartement = $IdDepartement;
+    }
+
+
+    public function SetPerson($tableName = 'doctors')
+    {
+        try {
+            $this->conection = new database();
+            $sql = "INSERT INTO $tableName (first_name,last_name,specialization,phone_number,email,id_department) 
+        VALUES ( :first_name, :last_name,:specialization,:phone_number , :email, :id_department); ";
+            $stm = $this->conection->conn->prepare($sql);
+            $stm->bindParam('first_name', $this->FirstName);
+            $stm->bindParam('last_name', $this->LastName);
+            $stm->bindParam('specialization', $this->specialization);
+            $stm->bindParam('phone_number', $this->PhoneNumber);
+            $stm->bindParam('email', $this->email);
+            $stm->bindParam('id_department', $this->IdDepartement);
+            $stm->execute();
+            echo "ajouter est successful 👀\n";
+        } catch (Exception $error) {
+            echo 'error de ajoute !!';
+        }
+    }
+
 }
 
 class departement
@@ -92,23 +129,23 @@ class departement
     public $DepartementName;
     public $location;
     public $conection;
-    
 
 
-    public function __construct($deparname=null, $location=null)
+
+    public function __construct($deparname = null, $location = null)
     {
-        
+
         $this->DepartementName = $deparname;
         $this->location = $location;
     }
     public function getDepartement()
     {
-        $this->conection =  new database();
+        $this->conection = new database();
         return $this->conection->conn->query("SELECT * FROM departments ;")->fetchAll(PDO::FETCH_ASSOC);
     }
     public function setDepartement()
     {
-        $this->conection =  new database();
+        $this->conection = new database();
         $DepartementName = $this->DepartementName;
         $location = $this->location;
         try {
@@ -118,31 +155,33 @@ class departement
             $stm->execute();
 
             echo "l'ajoute est successfull";
-        }catch(Exception $e){
+        } catch (Exception $e) {
             echo "try again ...";
         }
     }
 
-    public function ModifierDepartement($id,$choix,$change){
-        try{
-            $this->conection =  new database();
+    public function ModifierDepartement($id, $choix, $change)
+    {
+        try {
+            $this->conection = new database();
             $stm = $this->conection->conn->prepare("UPDATE departments SET $choix=:change WHERE id_department =:id");
-        $stm->bindParam('change',$change);
-        $stm->bindParam('id',$id);
-        $stm->execute();
-        }catch(Exception $e){
+            $stm->bindParam('change', $change);
+            $stm->bindParam('id', $id);
+            $stm->execute();
+        } catch (Exception $e) {
             echo "modufication failed try again !!";
         }
     }
     public function getDepartementId($id)
     {
-        $this->conection =  new database();
+        $this->conection = new database();
         return $this->conection->conn->query("SELECT * FROM departments WHERE id_department =$id;")->fetchAll(PDO::FETCH_ASSOC);
     }
-    public function supprimeDepartment($id){
-        $this->conection =  new database();
+    public function supprimeDepartment($id)
+    {
+        $this->conection = new database();
         $stm = $this->conection->conn->prepare("DELETE FROM `departments` WHERE id_department = :id");
-        $stm->bindParam('id',$id);
+        $stm->bindParam('id', $id);
         $stm->execute();
     }
 }
